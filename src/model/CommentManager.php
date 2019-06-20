7b217595769a42544fcdbcf865d1e6c2bd8082c4
N3ll3 <ge.fulldev.web@gmail.com>
2019-06-20 15:54:40 +0200

    css ok

--- src/model/CommentManager.php
+++ src/model/CommentManager.php
@@ -8,7 +8,7 @@ class CommentManager extends Manager
     public function getComments($postId)
     {
         $db = $this->dbConnect();
-        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = :post_id ORDER BY comment_date DESC');
+        $comments = $db->prepare('SELECT id, author, comment, flagged, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = :post_id ORDER BY comment_date DESC');
         $comments->execute(array('post_id' => $postId));
 
         return $comments;
